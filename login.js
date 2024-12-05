0329d698c8da65524e21c6c11db4e62c3714185f
Briant Jasper <briantjasper5@gmail.com>
2024-12-05 20:03:10 +0700

    Login Function

---
+++ login.js
@@ -0,0 +1,9 @@
+let username = "briantjasper";
+let password = "briant123";
+function login(username, password) {
+  if (username == username && password == password) {
+    console.log("Login Successfull");
+  }
+}
+
+login("briantjasper", "briant123");
